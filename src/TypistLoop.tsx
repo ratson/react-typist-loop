100975cdc50679315bdf70d519f361255a381a83
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2021-04-19 11:46:48 -0300

    Use TypeScript (#4)

---
+++ src/TypistLoop.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+export interface TypistLoopProps {
+  interval: number;
+  children: any;
+}
+
+const TypistLoop: React.FC<TypistLoopProps> = (
+  { interval = 1000, children },
+) => {
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+  const [mounted, setMounted] = React.useState<boolean>(false);
+  const [timer, setTimer] = React.useState<React.SetStateAction<any>>();
+
+  React.useEffect(() => {
+    setMounted(true);
+    return () => {
+      setMounted(false);
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, []);
+
+  const showNext = () => {
+    if (!mounted) {
+      return;
+    }
+    setCurrentIndex((currentIndex + 1) % React.Children.count(children));
+  };
+
+  const onTypingDone = React.useCallback(() => {
+    setTimer(setTimeout(showNext, interval));
+  }, []);
+
+  return React.Children.map(
+    children,
+    (child: any, i) =>
+      i === currentIndex &&
+      React.cloneElement(child, { onTypingDone }),
+  );
+};
+
+export default TypistLoop;
---
+++ test/TypiestLoop.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import TypistLoop from "../src/TypistLoop";
+
+test("renders correctly", () => {
+  const App: React.FC = () => (
+    <TypistLoop interval={3000}>
+      {["Hello World", "Good Morning", "Bye"].map((text) => (
+        <Typist key={text} startDelay={1000}>
+          {text}
+        </Typist>
+      ))}
+    </TypistLoop>
+  );
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
